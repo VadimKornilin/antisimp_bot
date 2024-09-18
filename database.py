0fafa6a610211c2ed4398e163d5435e0abfacfa0
kamen <vadim.kornil@yandex.ru>
2024-09-19 00:21:49 +0300

    Добавлена функция для внесения валидации в БД

--- database.py
+++ database.py
@@ -24,3 +24,23 @@ def db_insert(user_id, user_name, dt, subject, object, description):
     with postgres_connection() as con:
         con.execute(query)
         con.commit()
+
+
+def db_validate(user_id, user_name, dt, subject, object, description, validator, is_validated):
+    txt = f"""
+    update antisimp.simpings
+    set validator = '{validator}',
+    is_validated = '{is_validated}'
+    where 
+    user_id = {user_id}
+    and user_name = '{user_name}'
+    and dt = '{dt}'
+    and subject = '{subject}'
+    and object = '{object}'
+    and description = '{description}'
+    """
+
+    query = sqla.text(txt)
+    with postgres_connection() as con:
+        con.execute(query)
+        con.commit()
